df7455cd1acff075e8243b32b14465c9aada31dc
Munish Bali <mbali7@my.centennialcollege.ca>
2024-02-24 17:58:35 -0500

    Initial commit

---
+++ src/main/java/com/jenproject/App.java
@@ -0,0 +1,19 @@
+/*
+Student Id: 301239076
+Student name: Munish Bali
+Course: Comp228 Section 017
+Assignment: 
+*/
+package com.jenproject;
+
+/**
+ * @author munis
+ *
+ */
+public class App {
+	
+ public static void main(String[] args) {
+	System.out.println("Welcome to COMP367");
+}
+
+}
